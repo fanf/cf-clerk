428b279fc07c289c8cfd3a5853962e174e1d2e32
fanf <francois.armand@normation.com>
2013-04-09 10:37:35 +0200

    Remove the use of implicit from string + indent

--- src/main/scala/com/normation/cfclerk/xmlwriters/SectionSpecWriter.scala
+++ src/main/scala/com/normation/cfclerk/xmlwriters/SectionSpecWriter.scala
@@ -14,9 +14,11 @@ trait SectionSpecWriter {
 
 class SectionSpecWriterImpl extends SectionSpecWriter {
 
-  implicit def stringToText(text:String):Node = Text(text)
 
-  private[this] def createXmlNode(label : String,children : Seq[Node]) : Elem =
+  private[this] def createXmlTextNode(label : String, content : String) : Elem =
+    <tag>{Text(content)}</tag>.copy(label = label)
+
+  private[this] def createXmlNode(label : String, children : Seq[Node]) : Elem =
     <tag>{children}</tag>.copy(label = label)
 
   def serialize(rootSection:SectionSpec):Box[NodeSeq] = {
@@ -38,11 +40,12 @@ class SectionSpecWriterImpl extends SectionSpecWriter {
 
   private[this] def serializeSection(section:SectionSpec):NodeSeq = {
     val children = (section.children.flatMap( serializeChild(_))/:NodeSeq.Empty)((a,b) => a ++ b)
-    val xml = createXmlNode(SECTION,children) %
-                Attribute(SECTION_NAME,Text(section.name),Null) %
-                Attribute(SECTION_IS_MULTIVALUED,Text(section.isMultivalued.toString),Null) %
-                Attribute(SECTION_IS_COMPONENT,Text(section.isComponent.toString),Null) %
-                Attribute(SECTION_IS_FOLDABLE,Text(section.foldable.toString),Null)
+    val xml = (   createXmlNode(SECTION,children)
+                % Attribute(SECTION_NAME,           Text(section.name),Null)
+                % Attribute(SECTION_IS_MULTIVALUED, Text(section.isMultivalued.toString),Null)
+                % Attribute(SECTION_IS_COMPONENT,   Text(section.isComponent.toString),Null)
+                % Attribute(SECTION_IS_FOLDABLE,    Text(section.foldable.toString),Null)
+              )
 
     // add ComponentKey attribute
     section.componentKey.map{key =>
@@ -64,14 +67,14 @@ class SectionSpecWriterImpl extends SectionSpecWriter {
         (label, valueLabel.valueslabels)
     }
 
-    val name            = createXmlNode(VAR_NAME,variable.name)
-    val description     = createXmlNode(VAR_DESCRIPTION,variable.description)
-    val longDescription = createXmlNode(VAR_LONG_DESCRIPTION,variable.longDescription)
-    val isUnique        = createXmlNode(VAR_IS_UNIQUE_VARIABLE,variable.isUniqueVariable.toString)
-    val isMultiValued   = createXmlNode(VAR_IS_MULTIVALUED,variable.multivalued.toString)
-    val checked         = createXmlNode(VAR_IS_CHECKED,variable.checked.toString)
-    val items = (valueLabels.map(serializeItem(_))/:NodeSeq.Empty)((a,b) => a ++ b)
-    val constraint = serializeConstraint(variable.constraint)
+    val name            = createXmlTextNode(VAR_NAME,               variable.name)
+    val description     = createXmlTextNode(VAR_DESCRIPTION,        variable.description)
+    val longDescription = createXmlTextNode(VAR_LONG_DESCRIPTION,   variable.longDescription)
+    val isUnique        = createXmlTextNode(VAR_IS_UNIQUE_VARIABLE, variable.isUniqueVariable.toString)
+    val isMultiValued   = createXmlTextNode(VAR_IS_MULTIVALUED,     variable.multivalued.toString)
+    val checked         = createXmlTextNode(VAR_IS_CHECKED,         variable.checked.toString)
+    val items           = (valueLabels.map(serializeItem(_))/:NodeSeq.Empty)((a,b) => a ++ b)
+    val constraint      = serializeConstraint(variable.constraint)
 
     val children = (  name
                    ++ description
@@ -87,19 +90,19 @@ class SectionSpecWriterImpl extends SectionSpecWriter {
   }
 
   private[this] def serializeItem(item:ValueLabel):NodeSeq = {
-    val value = createXmlNode(CONSTRAINT_ITEM_VALUE,item.value)
-    val label = createXmlNode(CONSTRAINT_ITEM_LABEL,item.label)
+    val value = createXmlTextNode(CONSTRAINT_ITEM_VALUE,item.value)
+    val label = createXmlTextNode(CONSTRAINT_ITEM_LABEL,item.label)
     val child = Seq(value,label)
 
     createXmlNode(CONSTRAINT_ITEM,child)
   }
 
   private[this] def serializeConstraint(constraint:Constraint): NodeSeq = {
-    val constraintType = createXmlNode(CONSTRAINT_TYPE,constraint.typeName)
-    val dflt = constraint.default.map(createXmlNode(CONSTRAINT_DEFAULT,_)).getOrElse(NodeSeq.Empty)
-    val empty = createXmlNode(CONSTRAINT_MAYBEEMPTY,constraint.mayBeEmpty.toString)
-    val regexp = createXmlNode(CONSTRAINT_REGEX,constraint.regex.pattern)
-    val children = Seq(constraintType,dflt,empty,regexp).flatten
+    val constraintType = createXmlTextNode(CONSTRAINT_TYPE,       constraint.typeName)
+    val empty          = createXmlTextNode(CONSTRAINT_MAYBEEMPTY, constraint.mayBeEmpty.toString)
+    val regexp         = createXmlTextNode(CONSTRAINT_REGEX,      constraint.regex.pattern)
+    val dflt           = constraint.default.map(createXmlTextNode(CONSTRAINT_DEFAULT,_)).getOrElse(NodeSeq.Empty)
+    val children       = Seq(constraintType,dflt,empty,regexp).flatten
 
     createXmlNode(VAR_CONSTRAINT,children)
   }
